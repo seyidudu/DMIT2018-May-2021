429853b072fc38833de3dd8b1eb25edb1b2c8dac
seyidudu <59752314+seyidudu@users.noreply.github.com>
2021-05-06 16:13:54 -0600

    Add Bootsrap Menu to Site Master, Update Nuget Packages and Create Artist Entity

---
+++ ChinookSolution/ChinookSystem/Entities/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additiional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Entities
+{
+    internal class Album
+    {
+    }
+}
