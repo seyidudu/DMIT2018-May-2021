32899ccc41a273d8bf9bc69c850fcc057409334c
seyidudu <59752314+seyidudu@users.noreply.github.com>
2021-05-12 19:30:39 -0600

    Added Tracks Entity and edited Albums Entity

--- ChinookSolution/ChinookSystem/Entities/Album.cs
+++ ChinookSolution/ChinookSystem/Entities/Album.cs
@@ -7,23 +7,33 @@ namespace ChinookSystem.Entities
 {
     internal partial class Album
     {
+        private string _ReleaseLabel;
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Album()
+        {
+            Tracks = new HashSet<Track>();
+        }
 
         public int AlbumId { get; set; }
 
-        [Required(ErrorMessage = "Album Title is required")]
-        [StringLength(160, ErrorMessage = "Album Title is limited to 160 Characters.")]
+        [Required(ErrorMessage = "Album Title is Required")]
+        [StringLength(160, MinimumLength = 1, ErrorMessage = "Album title is limited to 160 characters.")]
         public string Title { get; set; }
 
         public int ArtistId { get; set; }
 
         public int ReleaseYear { get; set; }
 
-        [StringLength(50, ErrorMessage = "Album Release label is limited to 50 Characters.")]
-        public string ReleaseLabel { get; set; }
+        [StringLength(50, ErrorMessage = "Album release label is limited to 50 characters.")]
+        public string ReleaseLabel
+        {
+            get { return _ReleaseLabel; }
+            set { _ReleaseLabel = string.IsNullOrEmpty(value) ? null : value; }
+        }
 
-        //Navigational properties
-        //Navigational properties create a virtual
-        public virtual Artist Artist { get; set; }// parent
+        public virtual Artist Artist { get; set; }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Track> Tracks { get; set; }
     }
 }
